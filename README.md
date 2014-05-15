38b862b2f1abd90a9d1f1e83b9632e07350399b4
Matt Copperwaite <mattcopp+git@gmail.com>
2014-05-15 21:03:18 +0100

    added basic administration app

---
+++ README.md
@@ -0,0 +1,25 @@
+# Displays list of active members of the South London Makerspace app and allows administrators to manage them
+A module for `slm-site`.
+
+Displays the administration page for the main site.
+
+# Usage
+Requires `adminemail` setting in config and uses that e-mail address as the original admin. That user can then add permission to other users.
+
+# License
+
+    South London Makerspace Members List
+    Copyright (C) 2014  Matt Copperwaite
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ app.js
@@ -0,0 +1,119 @@
+var express = require('express'),
+    _ = require("underscore"),
+    swig = require("swig");
+
+module.exports = {
+    "title": "Admin",
+    "name": "admin",
+    "routes": [],
+    "app": function admin (config, db, site) {
+        var app = express();
+        _.extend(app.locals, site.locals);
+        
+        app.set('views', __dirname + "/views");
+        
+        app.get('/member/:uuid', function index (req, res) {
+            var user = res.locals.user;
+            if (user) {
+                if ((user.permissions >= 50) || (user.email == config.adminemail)) {
+                    // get all members
+                    res.locals.User.findOne({where: {uuid: req.params.uuid}},
+                        function (err, member) {
+                            if (!err) {
+                                member.historic_events(function (err, events) {
+                                    if (!err) {
+                                        res.render("member",{member: member, events: events});
+                                    }
+                                    else {
+                                        res.locals.flash("danger", "Database Error.", "Member evens cannot be retrieved at present.");
+                                        res.redirect("/admin");
+                                    }
+                                });
+                            }
+                            else {
+                                res.locals.flash("danger", "Database Error.", "Member cannot be retrieved at present.");
+                                res.redirect("/admin");
+                            }
+                        }
+                    );
+                }
+                else {
+                    res.locals.flash("danger", "Not enough permissions.", "You do not have enough permissions for admin access.");
+                    res.redirect("/admin");
+                }
+            }
+            else {
+                res.locals.flash("danger", "Not logged in.", "You cannot access the admin list when not logged in.");
+                res.redirect("/admin");
+            }
+        });
+        
+        app.get('/', function index (req, res) {
+            var user = res.locals.user;
+            if (user) {
+                if ((user.permissions >= 50) || (user.email == config.adminemail)) {
+                    // get all members
+                    res.locals.User.all(function (err, members) {
+                        if (!err) {
+                            res.render("members",{members: members, members_count: members.length});
+                        }
+                        else {
+                            res.locals.flash("danger", "Could not write to database.", "There was an error with the database.");
+                            res.redirect("/admin");
+                        }
+                    });
+                }
+                else {
+                    res.locals.flash("danger", "Not enough permissions.", "You do not have enough permissions for admin access.");
+                    res.redirect("/");
+                }
+            }
+            else {
+                res.locals.flash("danger", "Not logged in.", "You cannot access the admin list when not logged in.");
+                res.redirect("/");
+            }
+        });
+        
+        app.post("/member/:uuid", function (req, res) {
+            var user = res.locals.user;
+
+            if (user) {
+                if ((user.permissions >= 50) || (user.email == config.adminemail)) {
+                    // update member
+                    res.locals.User.findOne({where: {uuid: req.params.uuid}}, function (err, member) {
+                        member.name = req.body.name;
+                        member.email = req.body.email;
+                        member.address = req.body.address;
+                        member.card_id = req.body.card_id;
+                        member.last_payment = Date.parse(req.body.last_payment);
+                        member.permission = parseInt(req.body.permission);
+                
+                        member.isValid(function (valid) {
+                            if (valid) {
+                                member.save(function (err, member) {
+                                    // must handle validation errors
+                                    if (!err) {
+                                        res.locals.flash("success", "Updated.", "Member account updated successfully.");
+                                        res.render("member", {member: member});
+                                    }
+                                    else {
+                                        console.log("Could not save entry because: " + err);
+                                        console.log("Data: " + member);
+                                        res.send(500, "Database error. This has been logged but please report the issue with the code SLME003.");
+                                    }
+                                });
+                            }
+                            else {
+                                res.locals.flash("danger", "Update failed.", "Member information could not be saved because of errors.");
+                                res.render("membership", {member: member, errors: member.errors});
+                            }
+                        });
+                    });
+                }
+            }
+        });
+        
+        return app;
+    }
+}
+
